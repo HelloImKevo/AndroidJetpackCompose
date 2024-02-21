80fbdb03e971f825fff16cf73b832abde3ccbaf1
Kevin Schanz <Kevin.Schanz@globalpay.com>
2024-02-21 18:15:30 -0500

    [ToDoList] Use IDE-generated Jetpack Compose project boilerplate

---
+++ ToDoList/app/src/main/java/com/schanz/todolist/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.schanz.todolist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.schanz.todolist.ui.theme.ToDoListTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoListTheme {
+            }
+        }
+    }
+}
