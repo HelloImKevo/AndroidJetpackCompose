1ec069a01791e31869e069c1e1b6c97a5ea6387e
Kevin Schanz <Kevin.Schanz@globalpay.com>
2024-02-22 19:27:13 -0500

    [ToDoList] Setup Dagger-Hilt for RoomDatabase

--- ToDoList/app/src/main/java/com/schanz/todolist/data/ToDoDatabase.kt
+++ ToDoList/app/src/main/java/com/schanz/todolist/data/ToDoDatabase.kt
@@ -1,10 +1,11 @@
 package com.schanz.todolist.data
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.schanz.todolist.data.models.ToDoTask
 
 @Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
-abstract class ToDoDatabase {
+abstract class ToDoDatabase : RoomDatabase() {
 
     abstract fun toDoDao(): ToDoDao
 }
